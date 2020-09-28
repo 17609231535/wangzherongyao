3de6345790638dc1e25f24445de6d111542e601e
17609231535 <1519153391@qq.com>
2020-09-28 08:36:48 +0800

    后台第一次完整提交

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="app">
+    <!-- 这里承载的是所有页面的总停车位 -->
+    <router-view></router-view>
+  </div>
+</template>
+
+<style>
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+</style>
